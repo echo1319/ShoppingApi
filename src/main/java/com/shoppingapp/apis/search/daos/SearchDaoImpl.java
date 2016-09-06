c407bdb6b8d42878950f98e07cb916dfaf286118
ggkats` <glykeria.katsari@camelotglobal.com>
2016-09-06 20:55:21 +0300

    fixed query for products

--- src/main/java/com/shoppingapp/apis/search/daos/SearchDaoImpl.java
+++ src/main/java/com/shoppingapp/apis/search/daos/SearchDaoImpl.java
@@ -13,7 +13,6 @@ import java.util.List;
 
 @Repository
 public class SearchDaoImpl implements SearchDao {
-
     private JdbcTemplate jdbcTemplate;
 
     @Autowired
@@ -23,24 +22,21 @@ public class SearchDaoImpl implements SearchDao {
 
     @Override
     public List<Store> getStoresForProduct(String keyword, OrderBy orderBy) {
-
-        String sql = "select  s.store_id ,s.name ,s.address ,s.country , sp.price " +
-                " from product p ,store_product sp, store s" +
+        String sql = " select  s.store_id ,s.name ,s.address ,s.country , sp.price " +
+                " from product p ,store_product sp, store s " +
                 " where  LOWER(p.name) like '%" + keyword.toLowerCase() + "%' and sp.store_id=s.store_id " +
-                "and sp.product_id= p.product_id ";
+                " and sp.product_id= p.product_id ";
 
         if (orderBy == OrderBy.PRICE) {
             sql += " order by sp.price asc ";
         }
 
         List<Store> stores = jdbcTemplate.query(sql, new BeanPropertyRowMapper(Store.class));
-
         return stores;
     }
 
     @Override
     public List<Store> getStores(String keyword, OrderBy orderBy) {
-
         String sql = "select * from store s where LOWER(s.name) like '%" + keyword.toLowerCase() + "%'";
 
         if (orderBy == OrderBy.NAME) {
@@ -51,13 +47,12 @@ public class SearchDaoImpl implements SearchDao {
         return stores;
     }
 
-
     @Override
     public List<Product> getProduct(String keyword, String category, OrderBy orderBy) {
 
-        String sql = "select DISTINCT p.product_id, p.name, p.description, p.category, sp.price " +
-                "from product p, store_product sp  where sp.product_id=p.product_id " +
-                "and  LOWER(p.name) like '%" + keyword.toLowerCase() + "%'";
+        String sql = "select p.name, p.category, p.description, t.price from product p , " +
+                "( select  min(sp.price) as price , sp.product_id from store_product sp group by sp.product_id ) as t " +
+                " where t.product_id=p.product_id and  LOWER(p.name) like '%" + keyword.toLowerCase() + "%' ";
 
         if (!category.isEmpty()) {
             sql += " GROUP by p.category ";
@@ -72,10 +67,6 @@ public class SearchDaoImpl implements SearchDao {
         }
 
         List<Product> products = jdbcTemplate.query(sql, new BeanPropertyRowMapper(Product.class));
-
         return products;
-
     }
-
-
 }
